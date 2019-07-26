23d8f343c06a56840a7811a77e721be0b067abf6
ellis-ASC5 <ebuery0728@bths.edu>
2019-07-26 11:31:34 -0400

    try2

---
+++ Login.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('loginbutton').onclick = function() {
+        var user = document.getElementById('username').value;
+    var pass = document.getElementById('password').value; 
+    if (user == "Ellis" && pass == "asc"){
+        alert('Welcome Ellis');
+        window.location.href = 'homepage.html';
+        localStorage.setItem('username',user);
+        localStorage.setItem('password',pass);
+    };
+        return false;
+    };
+});
---
+++ homepage.js
@@ -0,0 +1,2 @@
+console.log(localStorage.getItem('username'));
+console.log(localStorage.getItem('password'));
